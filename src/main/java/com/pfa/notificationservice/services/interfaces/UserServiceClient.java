9c6d081e0deaa816e14699099548d5fe1f99331e
apostorial <t.elouadrassi@gmail.com>
2025-01-16 06:08:19 +0100

    Created an interface to house the needed Feign methods.

--- src/main/java/com/pfa/notificationservice/services/interfaces/UserServiceClient.java
+++ src/main/java/com/pfa/notificationservice/services/interfaces/UserServiceClient.java
@@ -6,13 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Set;
-import java.util.UUID;
 
 @FeignClient(name = "user-service", url = "${user-service.url}")
 public interface UserServiceClient {
     @GetMapping("/api/public/users/{sellerId}/followers")
-    Set<String> getFollowers(@PathVariable UUID sellerId);
+    Set<String> getFollowers(@PathVariable String sellerId);
 
     @GetMapping("/api/public/users/{userId}")
-    DataUserResponse getUserById(@PathVariable("userId") UUID userId);
+    DataUserResponse getUserById(@PathVariable String userId);
 }
